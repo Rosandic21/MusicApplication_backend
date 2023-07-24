209b910622d880faebbd197bae0f2e4411663ff4
Daniel Rosandic <daniel@Daniels-MBP.attlocal.net>
2023-07-24 08:48:29 -0400

    SpotifyRemixed with sample foo API to test fetching

--- server/index.js
+++ server/index.js
@@ -1,58 +1,11 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res)=> {
-// Build file path
-let filePath = path.join(__dirname, '../client/public', req.url === '/' ? 'index.html' : req.url);
-
-// Get the extension of the file
-let extname = path.extname(filePath);
-
-// Initial content type
-let contentType = 'text/html';
-
-// Check file extension and set content type
-switch(extname){
-    case '.js':
-        contentType = 'text/javascript';
-        break;
-    case '.css':
-        contentType = 'text/css';
-        break;
-    case '.json':
-        contentType = 'application/json';
-        break;
-    case '.png':
-        contentType = 'image/png';
-        break;
-    case '.jpg':
-        contentType = 'image/jpg';
-        break;
-}
-    // read file
-    fs.readFile(filePath, (err,content)=> {
-        if(err){
-            if(err.code == 'ENOENT') { // if page not found error code is recieved then load 404 error
-                fs.readFile(path.join(__dirname,'../client/public', '404.html'),
-                (err, content)=> {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.end(content, 'utf8');
-                })
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`);
-                }
-            }   
-        else {
-            // success --  load the content
-            res.writeHead(200, {'Content-Type': contentType});
-            res.end(content, 'utf8');
-        }
-    });
-
+// route the HTTP GET requests to root '/api'
+app.get("/api", (req, res) => {
+   // res.send("Get request called");
+    res.json({ "users": ["userOne", "userTwo", "userThree"] });
 });
 // for the function above, we first need to listen on the port for the HTTP get request
-const PORT = process.env.PORT || 3000; // look for environment variable (hosts default port) and if not found then assign port 3000
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+const PORT = process.env.PORT || 5000; // look for environment variable (hosts default port) and if not found then assign port 5000
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
